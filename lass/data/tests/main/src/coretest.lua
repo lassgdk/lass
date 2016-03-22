24d6d85bab2edc3c217cb661a9df65ecafd15ba5
Nikki <nikkicubed@gmail.com>
2016-03-22 17:26:37 -0600

    reformatted coretests to match standard

--- lass/data/tests/main/src/coretest.lua
+++ lass/data/tests/main/src/coretest.lua
@@ -4,34 +4,35 @@ local geometry = require("lass.geometry")
 local coretest = {}
 
 local function testLocalPosition(object, assertedPosition)
+
 	local s1, s2 = tostring(assertedPosition), tostring(object.transform.position)
 	assert(
 		object.transform.position == assertedPosition,
 		"local transform position should be" .. s1 .. " but is " .. s2
 	)
+
 end
 
 local function testGlobalPosition(object, assertedPosition)
+
 	local s1, s2 = tostring(assertedPosition), tostring(object.globalTransform.position)
 	assert(
 		object.globalTransform.position == assertedPosition,
 		"global transform position should be" .. s1 .. " but is " .. s2
 	)
+
 end
 
 function coretest.testGameObjectMovement(scene)
 
 	--[[setup]]
-
 	local object = lass.GameObject(scene, "test")
-	local child = lass.GameObject(scene, "test child")
 
 	testLocalPosition(object, geometry.Vector3(0,0,0))
 	testGlobalPosition(object, geometry.Vector3(0,0,0))
 
 
 	--[[GameObject.move]]
-
 	object:move(5,5)
 	object:move(0,1)
 	testLocalPosition(object, geometry.Vector3(5,6,0))
@@ -43,7 +44,6 @@ function coretest.testGameObjectMovement(scene)
 
 
 	--[[GameObject.moveTo]]
-
 	object:moveTo(-2,10)
 	testLocalPosition(object, geometry.Vector3(-2,10,0))
 	testGlobalPosition(object, geometry.Vector3(-2,10,0))
@@ -54,7 +54,6 @@ function coretest.testGameObjectMovement(scene)
 
 
 	--[[GameObject.moveGlobal]]
-
 	object:moveGlobal(5,5)
 	object:moveGlobal(0,1)
 	testLocalPosition(object, geometry.Vector3(5,6,0))
@@ -66,7 +65,6 @@ function coretest.testGameObjectMovement(scene)
 
 
 	--[[GameObject.moveToGlobal]]
-
 	object:moveToGlobal(-2,10)
 	testLocalPosition(object, geometry.Vector3(-2,10,0))
 	testGlobalPosition(object, geometry.Vector3(-2,10,0))
@@ -74,12 +72,12 @@ function coretest.testGameObjectMovement(scene)
 	object:moveToGlobal(0,0)
 	testLocalPosition(object, geometry.Vector3(0,0,0))
 	testGlobalPosition(object, geometry.Vector3(0,0,0))
+
 end
 
 function coretest.testGameObjectChildMovement(scene)
 
 	--[[setup]]
-
 	local object = lass.GameObject(scene, "test")
 	local child = lass.GameObject(scene, "test child")
 	object:addChild(child)
@@ -89,7 +87,6 @@ function coretest.testGameObjectChildMovement(scene)
 
 
 	--[[GameObject.move]]
-
 	object:move(5,5)
 	child:move(0,1)
 	testLocalPosition(child, geometry.Vector3(0,1,0))
@@ -102,7 +99,6 @@ function coretest.testGameObjectChildMovement(scene)
 
 
 	--[[GameObject.moveTo]]
-
 	object:moveTo(10,10)
 	child:moveTo(-2,0)
 	testLocalPosition(child, geometry.Vector3(-2,0,0))
@@ -118,7 +114,6 @@ function coretest.testGameObjectChildMovement(scene)
 
 
 	--[[GameObject.moveGlobal]]
-
 	object:moveGlobal(5,5)
 	child:moveGlobal(0,1)
 	testLocalPosition(child, geometry.Vector3(0,1,0))
@@ -134,7 +129,6 @@ function coretest.testGameObjectChildMovement(scene)
 
 
 	--[[GameObject.moveToGlobal]]
-
 	object:moveToGlobal(-2,10)
 	child:moveToGlobal(5,5)
 	testLocalPosition(child, geometry.Vector3(7,-5,0))
@@ -147,6 +141,13 @@ function coretest.testGameObjectChildMovement(scene)
 	child:moveToGlobal(0,0)
 	testLocalPosition(child, geometry.Vector3(0,0,0))
 	testGlobalPosition(child, geometry.Vector3(0,0,0))
+
+end
+
+function coretest.testGameObjectChildTransform
+
+	--[[setup]]
+
 end
 
 return coretest
