c00fd9815c962eb8f72e3bedf115e2f16957a3ee
Nikki <nikkicubed@gmail.com>
2016-05-14 17:04:14 -0600

    Issue #54: fixed assertIncorrectSetting not working correctly when given a nested table for var

--- lass/data/tests/main/src/tests/geometrytest/helpers.lua
+++ lass/data/tests/main/src/tests/geometrytest/helpers.lua
@@ -1,50 +1,91 @@
 local helpers = {}
 
-function helpers.assertIncorrectValues(geometryClass, className, variables, default, useNegative, useTable)
+local function repr(x)
 
-    local badValues = {"1", false, math.huge, -math.huge, math.huge / math.huge}
-
-    -- sometimes negative values are allowed, so this is optional
-    if useNegative then
-        table.insert(badValues, -1)
+    if type(x) == "string" then
+        return string.format("%q", x)
+    else
+        return tostring(x)
     end
 
-    -- sometimes table values, so this is optional
-    if useTable then
-        table.insert(badValues, {})
+end
+
+local function assertIncorrectCreation(i, var, incorrectValue, params, geometryClass, numVars, className)
+
+    params[i] = incorrectValue
+
+    -- attempt to make the class with a single incorrect value
+    -- need to give unpack the number of variables, because if nil is the default,
+    -- the unpack will potentially skip the nil values which can cause problems
+    local success, result = pcall(geometryClass, unpack(params, 1, numVars))
+    -- debug.log(result)
+    if success then
+        error(className .. "." .. var .. " incorrectly created with " .. repr(incorrectValue))
     end
 
-    for _, badValue in ipairs(badValues) do
+    -- params[i] = default
+end
+
+local function assertIncorrectSetting(var, incorrectValue, params, geometryClass, className)
+
+    local instance = geometryClass(unpack(params))
+    local varName = var
+    local success = nil
+    local result = nil
+
+    -- if var points us to a subvalue, dig it out
+    if type(var) == "table" then
+
+        varName = ""
+        for j = 1, #var - 1 do
+            varName = varName .. var[j] .. "."
+            instance = instance[var[j]]
+        end
+        varName = varName .. var[#var]
+        
+        -- attempt to set a nested value to something incorrect
+        success, result = pcall(function() instance[var[#var]] = incorrectValue end)
+    else
+        -- attempt to set a value to something incorrect
+        success, result = pcall(function() instance[var] = incorrectValue end)
+    end
+
+    -- debug.log(result)
+    if success then
+        error(className .. "." .. varName .. " incorrectly set to " .. repr(incorrectValue))
+    end
+end
+
+function helpers.assertIncorrectRunner(testType, geometryClass, className, variables, default, extraValues)
+
+    local incorrectValues = {"1", false, math.huge, -math.huge, math.huge / math.huge}
+
+    if extraValues then
+        for _, extraValue in ipairs(extraValues) do
+            table.insert(incorrectValues, extraValue)
+        end
+    end
+
+    for _, incorrectValue in ipairs(incorrectValues) do
 
         local params = {}
         for i, _ in ipairs(variables) do
             params[i] = default
         end
 
-        local instance = geometryClass(unpack(params))
         for i, var in ipairs(variables) do
 
-            -- attempt to set a value to something incorrect
-            local success, result = pcall(function() instance[var] = badValue end)
-            -- debug.log(result)
-            if success then
-                error(className .. "." .. var .. " incorrectly set to " .. tostring(badValue))
+            if testType == "creation" then
+                assertIncorrectCreation(i, var, incorrectValue, params, geometryClass, #variables, className)
+            elseif testType == "setting" then
+                assertIncorrectSetting(var, incorrectValue, params, geometryClass, className)
+            else
+                error("Incorrect test type parameter")
             end
 
-            params[i] = badValue
-
-            -- attempt to make the class with a single incorrect value
-            -- need to give unpack the number of variables, because if nil is the default,
-            -- the unpack will potentially skip the nil values which can cause problems
-            success, result = pcall(geometryClass, unpack(params, 1, #variables))
-            -- debug.log(result)
-            if success then
-                error(className .. "." .. var .. " incorrectly created with " .. tostring(badValue))
-            end
-
-            params[i] = default
         end
     end
+
 end
 
 return helpers
