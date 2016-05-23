b7454cedab74e363bf0bb19c78876e4d0948283d
Nikki <nikkicubed@gmail.com>
2016-05-22 19:24:42 -0600

    Issue #77: moved GameObjectTest:testDestroy into objectRemovalTestRunner, and replaced :destroy() calls with a modular function call based on given parameters

--- lass/data/tests/main/src/tests/coretest/helpers.lua
+++ lass/data/tests/main/src/tests/coretest/helpers.lua
@@ -48,4 +48,100 @@ function m.numTreeNodes(entity)
     return count
 end
 
+local function objectRemoval(scene, functionType, gameObject, destroyDescendants)
+
+    if functionType == "object" then
+        gameObject:destroy(destroyDescendants)
+    elseif functionType == "scene" then
+        scene:removeGameObject(gameObject, destroyDescendants)
+    else
+        error("incorrect functionType value")
+    end
+
+end
+
+function m.objectRemovalTestRunner(scene, functionType)
+    -- functionType is either "object" or "scene", referring to
+    -- GameObject:destroy or GameScene:removeGameObject, respectively
+
+    --[[normal usage]]
+    local object = self:createEntity(scene, "testing object")
+
+    objectRemoval(scene, functionType, object)
+    assertFalse(object.active, "object was not deactivated")
+    assertEqual(helpers.searchTreeDepth(scene.children, object), nil)
+
+    -- a second call should produce no error
+    objectRemoval(scene, functionType, object)
+    assertFalse(object.active, "object was reactivated")
+
+
+    --[[test destroying all children of object]]
+    object = lass.GameObject(scene, "test")
+    local child = self:createEntity(scene, "test child", nil, object)
+    local child2 = self:createEntity(scene, "test child2", nil, object)
+    local grandchild = self:createEntity(scene, "test grandchild", nil, child)
+
+    objectRemoval(scene, functionType, object, true)
+
+    assertEqual(child.active, false, "child was not deactivated")
+    assertEqual(helpers.searchTreeCount(scene.children, child), 0, "child was not removed from scene")
+
+    assertEqual(child2.active, false, "child2 was not deactivated")
+    assertEqual(helpers.searchTreeCount(scene.children, child2), 0, "child2 was not removed from scene")
+
+    assertEqual(grandchild.active, false, "grandchild was not deactivated")
+    assertEqual(
+        helpers.searchTreeDepth(scene.children, grandchild), nil, "grandchild was not removed from scene")
+
+
+    --[[destroy object without removing any children]]
+    object = self:createEntity(scene, "test")
+    child = self:createEntity(scene, "test child", nil, object)
+
+    objectRemoval(scene, functionType, object, false)
+    assertEqual(child.active, true, "child was incorrectly deactivated")
+    assertEqual(helpers.searchTreeDepth(scene.children, child), 1, "child was not made a child of the scene")
+    assertEqual(helpers.searchTreeCount(scene.children, child), 1, "child reference count is incorrect")
+
+    -- this shouldn't do anything, since the object was already destroyed
+    objectRemoval(scene, functionType, object, true)
+    assertEqual(child.active, true, "child was incorrectly deactivated")
+
+    -- try destroying the child in isolation
+    objectRemoval(scene, functionType, child)
+    assertEqual(child.active, false, "child was not deactivated")
+    assertEqual(helpers.searchTreeCount(scene.children, child), 0, "child was not removed from scene")
+
+    -- a second call should produce no error
+    child:destroy()
+    objectRemoval(scene, functionType, child)
+    assertFalse(child.active, "child was reactivated somehow")
+
+
+    --[[destroy child object without destroying descendants]]
+    object = lass.GameObject(scene, "test")
+    child = self:createEntity(scene, "test child", nil, object)
+    grandchild = self:createEntity(scene, "test grandchild", nil, child)
+
+    objectRemoval(scene, functionType, child, false)
+
+    assertEqual(object.active, true, "object was incorrectly deactivated")
+    assertEqual(helpers.searchTreeDepth(scene.children, object), 1, "object was not left a child of the scene")
+    assertEqual(helpers.searchTreeCount(scene.children, object), 1, "object reference count is incorrect")
+
+    assertEqual(child.active, false, "child was not deactivated")
+    assertEqual(helpers.searchTreeCount(scene.children, child), 0, "child was not removed from scene")
+
+    assertEqual(grandchild.active, true, "grandchild was incorrectly deactivated")
+    assertEqual(
+        helpers.searchTreeDepth(scene.children, grandchild), 1,
+        "grandchild was not made a child of the scene")
+    assertEqual(
+        helpers.searchTreeCount(scene.children, grandchild), 1,
+        "grandchild reference count is incorrect")
+
+
+end
+
 return m
